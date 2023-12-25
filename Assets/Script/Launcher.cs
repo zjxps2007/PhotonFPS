864f236f4025d95024d8f58aee7fa9b38d3a60de
JoInHo <zjxps2007@gmail.com>
2023-12-26 02:33:33 +0900

    Multiplayer Room and Player

--- Assets/Script/Launcher.cs
+++ Assets/Script/Launcher.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
+
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
+    public static Launcher Instance;
+    
     [SerializeField] private TMP_InputField roomNameInputField;
     [SerializeField] private TMP_Text errorText;
     [SerializeField] private TMP_Text roomNameText;
+    [SerializeField] private Transform roomListContent;
+    [SerializeField] private GameObject roomListItemPrefab;
+    [SerializeField] private Transform PlayerListContent;
+    [SerializeField] private GameObject PlayerListItemPrefab;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +40,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("title");
         Debug.Log("Joined Lobby");
+        PhotonNetwork.NickName = "Player " + Random.Range(0, 1000).ToString("0000");
     }
 
     public void CreateRoom()
@@ -42,6 +58,13 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         MenuManager.Instance.OpenMenu("room");
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+        
+        Player[] players = PhotonNetwork.PlayerList;
+
+        for (int i = 0; i < players.Count(); i++)
+        {
+            Instantiate(PlayerListItemPrefab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -56,8 +79,32 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("loading");
     }
 
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
     public override void OnLeftRoom()
     {
         MenuManager.Instance.OpenMenu("title");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trams in roomListContent)
+        {
+            Destroy(trams.gameObject);
+        }
+
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(PlayerListItemPrefab, PlayerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
     }
 }
---
+++ Assets/Script/RoomListItem.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Realtime;
+using TMPro;
+using UnityEngine;
+
+public class RoomListItem : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    private RoomInfo info;
+    
+    public void SetUp(RoomInfo _info)
+    {
+        info = _info;
+        text.text = _info.Name;
+    }
+
+    public void OnClick()
+    {
+        Launcher.Instance.JoinRoom(info);
+    }
+}
