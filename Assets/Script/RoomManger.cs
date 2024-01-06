17ccf803dfac8388494dd749a372eff6417afb68
JoInHo <zjxps2007@gmail.com>
2024-01-06 20:33:45 +0900

    Add gameManage

---
+++ Assets/Script/RoomManger.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class RoomManger : MonoBehaviourPunCallbacks
+{
+    public static RoomManger Instance;
+
+    private void Awake()
+    {
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+        Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if (scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero,
+                Quaternion.identity);
+        }
+    }
+}
